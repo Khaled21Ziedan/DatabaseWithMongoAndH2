e716e3a1bf7e5d8875b0bef4882839ed62623e18
kmsaleh <khaledmohammedziedan@gmail.com>
2023-03-05 00:58:14 +0300

    last

--- src/main/java/com/example/Database/adapter/model/Account.java
+++ src/main/java/com/example/Database/adapter/model/Account.java
@@ -1,6 +1,5 @@
 package com.example.Database.adapter.model;
 
-import com.example.Database.adapter.repo.mysql.impl.User.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,5 +12,5 @@ import lombok.NoArgsConstructor;
 public class Account {
     private long id ;
     private String iban;
-    private User userEntity;
+    private User user;
 }
